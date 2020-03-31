dbf1b14a0736798b1f03c052492b6bf367699c4b
cidospark <cidospark27@gmail.com>
2020-04-01 00:36:04 +0100

    Added operations model repositories

---
+++ CalculatorLibrary/OperationsModel/IOperationsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary.OperationsModel
+{
+    /// <summary>
+    /// This interface provides a contract on the applications operations
+    /// </summary>
+    public interface IOperationsRepository
+    {
+        
+    }
+}
---
+++ CalculatorLibrary/OperationsModel/OperationsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary.OperationsModel
+{
+    /// <summary>
+    /// This class implements the operations interface thereby binding to the contract
+    /// </summary>
+    class OperationsRepository : IOperationsRepository
+    {
+        
+    }
+}
